3f689820764d119429426258512b1aebbeadf7bd
Ayman Nageeb <aimangta123121@gmail.com>
2024-07-21 09:57:54 +0200

    Set Main AppBar UI

---
+++ components/MainAppBar.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <v-app-bar scroll-behavior="hide elevate" height="100" color="white" class="px-4 ma-0">
+    <span>
+      <v-img rounded=xl width="75" height="75" src="@/assets/images/image.png"></v-img>
+    </span>
+    <v-container>
+      <div>
+
+        <v-toolbar color="transparent" height="100">
+
+          <v-spacer></v-spacer>
+          <v-btn size="x-large" rounded="pill" class="px-8" color="primary"
+            prepend-icon="mdi-invoice-text-outline">blog</v-btn>
+          <span class="mx-1"></span>
+          <v-btn size="x-large" rounded="pill" class="px-8" color="primary" prepend-icon="mdi-information-variant">about
+            us</v-btn>
+          <span class="mx-1"></span>
+          <v-btn size="x-large" rounded="pill" class="px-8" color="primary"
+            prepend-icon="mdi-land-fields">projects</v-btn>
+          <v-spacer></v-spacer>
+
+        </v-toolbar>
+      </div>
+    </v-container>
+    <v-btn color="success" variant="plain" icon="mdi-translate mdi-24px"></v-btn>
+    <span class="mx-1"></span>
+    <v-btn color="black" icon="mdi-menu" />
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,18 @@
 <template>
-  <v-app-bar >
-    the main app
-  </v-app-bar>
-  <v-main class="text-red text-h1">
-    Main content
-    <NuxtPage />
-    <slot />
+  <MainAppBar />
+  <v-main class="text-red text-h1" style="height: 200vh;">
+    <div style="height: 100vh; position: absolute; top: 0; left: 0; width: 100vw;padding-top: 120px;
+    background-repeat: no-repeat; 
+    background-position: center; 
+    background-size: cover;
+    background-image: url('http://localhost:3000/_nuxt/assets/images/intro-bg-image.png') ;">
+      <span class="font-weight-black text-h1">
+        data
+      </span>
+      <slot />
+
+    </div>
+
   </v-main>
   <v-footer>
     the main footer
