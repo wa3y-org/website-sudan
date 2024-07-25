1d973c97387fd5a111f6c334f6f651ff65badb8f
Ayman Nageeb <aimangta123121@gmail.com>
2024-07-25 11:51:42 +0200

    Fix: reorder data

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -2,28 +2,6 @@
   <v-footer class="main-footer py-12">
     <v-container class="">
       <v-row>
-
-        <v-col cols="12" xxl="4" xl="4" lg="4" md="6" sm="12" xs="12">
-
-          <div class="pa-8">
-            <h1 class="font-weight-black text-h3 mb-4">Organization</h1>
-            <hr class="mb-8"
-              style="width: 6rem; height: 0.3rem; background-color: #da291c; border: 0px; border-radius: 1rem;">
-            <p>
-            <ul class="links-group">
-              <li v-for="(link) of ourOrganizationLinks">
-                
-                    <nuxt-link class="our-link " :href="link.url">
-                      
-                      <span class="mx-2">
-                        {{ link.name }}
-                      </span>
-                    </nuxt-link>
-              </li>
-            </ul>
-            </p>
-          </div>
-        </v-col>
         <v-col cols="12" xxl="4" xl="4" lg="4" md="6" sm="12" xs="12">
           <div class="pa-8">
             <h1 class="font-weight-black text-h3 mb-4">Pages</h1>
@@ -43,6 +21,27 @@
           </div>
         </v-col>
         <v-col cols="12" xxl="4" xl="4" lg="4" md="6" sm="12" xs="12">
+
+          <div class="pa-8">
+            <h1 class="font-weight-black text-h3 mb-4">Organization</h1>
+            <hr class="mb-8"
+              style="width: 6rem; height: 0.3rem; background-color: #da291c; border: 0px; border-radius: 1rem;">
+            <p>
+            <ul class="links-group">
+              <li v-for="(link) of ourOrganizationLinks">
+
+                <nuxt-link class="our-link " :href="link.url">
+
+                  <span class="mx-2">
+                    {{ link.name }}
+                  </span>
+                </nuxt-link>
+              </li>
+            </ul>
+            </p>
+          </div>
+        </v-col>
+        <v-col cols="12" xxl="4" xl="4" lg="4" md="6" sm="12" xs="12">
           <div class="pa-8">
             <h1 class="font-weight-black text-h3 mb-4">Social Media</h1>
             <hr class="mb-8"
@@ -51,8 +50,10 @@
               <a :href="media.url" v-for="media of ourSocialMedia" class="ma-6">
                 <v-hover>
                   <template v-slot:default="{ isHovering, props }">
-                    <v-icon v-bind="props" :color="isHovering ? '#12095a' : '#d20015CC'">{{ media.Icon }}
-                      mdi-48px</v-icon>
+                    <span class="d-inline-block my-2">
+                      <v-icon v-bind="props" :color="isHovering ? '#12095a' : '#d20015CC'">{{ media.Icon }}
+                        mdi-48px</v-icon>
+                    </span>
                   </template>
                 </v-hover>
               </a>
@@ -111,7 +112,7 @@ const ourSocialMedia = [
   text-decoration: none;
   color: #12095a;
   list-style-type: none;
-  
+
   font-size: 1.25rem;
   text-shadow: 0 0 1px rgba(0, 0, 0, 0.01);
   text-rendering: optimizeLegibility;
