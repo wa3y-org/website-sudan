cb83f2fdd48f17349ced0365b7ecb0868e8b2f90
Ayman Nageeb <aimangta123121@gmail.com>
2024-07-28 13:38:20 +0200

    Feature: add articles cards

---
+++ components/BlogPage/ArticleCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-card class="my-8" elevation="3">
+      <v-img cover height="300" :src="article.cover_image"></v-img>
+      <v-card-text class="pa-8" style="height: 18rem;">
+        <p class="text-indigo font-weight-black">{{ article.topic.name }}</p>
+        <p class="my-4 text-h5 font-weight-bold">{{ article.title }}</p>
+        <p class="text-justify">{{ article.short_text }}</p>
+        <p class="mt-4 text-grey">
+          {{ article.author.name }}
+          <span class="mx-2">-</span>
+          {{ article.updated }}
+        </p>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Article } from "@/app/models/article";
+defineProps({
+  article: {
+    type: Article,
+    required: true,
+  }
+})
+
+
+</script>
+
+<style></style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,13 +1,51 @@
 <template>
   <div>
-    <coming-soon />
+    <h1 class="text-h3 text-center font-weight-black pt-16 pb-8">
+      Wa3y blog
+    </h1>
+
+    <v-container>
+      <div class="d-flex justify-center align-center">
+        <v-divider></v-divider>
+        <span class="font-weight-bold text-h6 text-success mx-6">Pinned</span>
+        <v-divider></v-divider>
+      </div>
+      <blog-page-pinned-article-card v-for="i in 1" :article="pinnedArticleExample1" />
+      <div class="d-flex justify-center align-center mt-4">
+        <v-divider></v-divider>
+        <span class="font-weight-bold text-h6 text-brown mx-6">Articles</span>
+        <v-divider></v-divider>
+      </div>
+      <v-row>
+        <v-col cols="12" xxl="3" xl="4" lg="6" md="6" v-for="i in 4">
+          <BlogPageArticleCard :article="pinnedArticleExample1" />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script>
 
 </script>
 
-<style>
+<script lang="ts" setup>
+import { Article } from "@/app/models/article";
+const pinnedArticles = [];
 
-</style>
+const pinnedArticleExample1 = new Article();
+pinnedArticleExample1.author = { name: "Ayman Nageeb" };
+pinnedArticleExample1.title = "Does Aromatherapy Works for Sleep? Maybe, Here's What We Know";
+pinnedArticleExample1.topic = { name: "Sleep Hygiene" };
+pinnedArticleExample1.short_text = `
+   Lorem ipsum dolor sit amet consectetur, 
+   adipisicing elit. Sit fuga, 
+   ipsa explicabo officiis corrupti eligendi repellendus est, 
+   voluptatibus saepe voluptates magnam nihil tempora? 
+   Ipsam facilis animi unde saepe, quisquam cumque.
+`;
+pinnedArticleExample1.updated = '2024-07-25'
+pinnedArticleExample1.cover_image = '/images/visuel-sensibilisation.jpg'
+</script>
+
+<style></style>
